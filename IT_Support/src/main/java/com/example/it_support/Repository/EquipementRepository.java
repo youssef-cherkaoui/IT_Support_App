3f89fbfdddbc0b7b13f385aad1229d1009530c0b
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-05 16:46:48 +0100

    Creation EquipRepository

---
+++ IT_Support/src/main/java/com/example/it_support/Repository/EquipementRepository.java
@@ -0,0 +1,7 @@
+package com.example.it_support.Repository;
+
+import com.example.it_support.Model.EquipementModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipementRepository extends JpaRepository<EquipementModel, Long> {
+}
