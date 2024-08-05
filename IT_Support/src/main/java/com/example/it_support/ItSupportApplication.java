26d0bd9ee1e55a01d98f400406aa8549ac686fd8
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-05 14:55:09 +0100

    creation du projet IT_Support

---
+++ IT_Support/src/main/java/com/example/it_support/ItSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSupportApplication.class, args);
+    }
+
+}
---
+++ IT_Support/src/test/java/com/example/it_support/ItSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
