4d06ad5f58cc9452c6a6b2345d33b6c26b28882b
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-06 18:02:37 +0100

    Creation Interface EquipementService

--- IT_Support/src/main/java/com/example/it_support/Service/EquipementService.java
+++ IT_Support/src/main/java/com/example/it_support/Service/EquipementService.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 public interface EquipementService {
 
-    EquipementModel createEquipement(EquipementModel equipement);
+    EquipementModel CreateEquipement(EquipementModel equipement);
     List<EquipementModel> getAllEquipements();
-    void  deleteEquipement(Long id);
-    EquipementModel editEquipement(EquipementModel equipement , Long id);
-
-    }
+    void deleteEquipements(Long idEquipement);
+    EquipementModel updateEquipements(EquipementModel equipement,Long idequipement);
+}
