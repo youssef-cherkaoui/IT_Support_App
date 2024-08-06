808f87679220c227f2cfce5c6298d5afd3df5c8a
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-06 18:02:58 +0100

    Creation L'implementation EquipementService

--- IT_Support/src/main/java/com/example/it_support/Service/EquipementServiceImpl.java
+++ IT_Support/src/main/java/com/example/it_support/Service/EquipementServiceImpl.java
@@ -1,12 +1,12 @@
 package com.example.it_support.Service;
 
-
 import com.example.it_support.Model.EquipementModel;
 import com.example.it_support.Repository.EquipementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EquipementServiceImpl implements EquipementService{
@@ -15,29 +15,38 @@ public class EquipementServiceImpl implements EquipementService{
     private EquipementRepository equipementRepository;
 
     @Override
-    public EquipementModel createEquipement(EquipementModel equipement){
+    public EquipementModel CreateEquipement(EquipementModel equipement) {
         return equipementRepository.save(equipement);
     }
 
+
     @Override
-    public List<EquipementModel> getAllEquipements(){
+    public List<EquipementModel> getAllEquipements() {
         return equipementRepository.findAll();
     }
 
-    @Override
-    public EquipementModel editEquipement(EquipementModel equipement , Long id){
-        equipementRepository.findById(id);
-        EquipementModel editEquipement = new EquipementModel();
-        editEquipement.setIdequipement(id);
-        editEquipement.setNom(equipement.getNom());
-        editEquipement.setDescription(equipement.getDescription());
-        editEquipement.setDateAquisation(equipement.getDateAquisation());
 
-        return equipementRepository.save(editEquipement);
+    @Override
+    public void deleteEquipements(Long idEquipement) {
+        equipementRepository.deleteById(idEquipement);
     }
 
     @Override
-    public void deleteEquipement(Long id){
-        equipementRepository.deleteById(id);
+    public EquipementModel updateEquipements(EquipementModel equipement,Long idequipement){
+
+        Optional<EquipementModel> existingEquipement = equipementRepository.findById(idequipement);
+        if(existingEquipement.isPresent()){
+            EquipementModel equipToUpdate = existingEquipement.get();
+            equipToUpdate.setNom(equipement.getNom());
+            equipToUpdate.setDescription(equipement.getDescription());
+            equipToUpdate.setEtatEquipement(equipement.getEtatEquipement());
+            equipToUpdate.setDateAquisation(equipement.getDateAquisation());
+            return equipementRepository.save(equipToUpdate);
+
+        }else{
+            throw new RuntimeException("Non trouvé"+idequipement);
+        }
     }
+
+
 }
