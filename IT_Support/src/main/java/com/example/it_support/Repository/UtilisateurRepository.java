1608459e8ad6422930e5f2b7b8dc2bb48b0a1d10
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-06 16:31:48 +0100

    Creation Interface UtilisRepo

---
+++ IT_Support/src/main/java/com/example/it_support/Repository/UtilisateurRepository.java
@@ -0,0 +1,7 @@
+package com.example.it_support.Repository;
+
+import com.example.it_support.Model.UtilisateurModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtilisateurRepository extends JpaRepository<UtilisateurModel,Long> {
+}
