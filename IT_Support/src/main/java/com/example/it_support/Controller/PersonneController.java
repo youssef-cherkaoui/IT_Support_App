ad34e4a11acff41a07b741a4ad4c0b26b666faac
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-06 16:26:21 +0100

    Creation PersonneController

---
+++ IT_Support/src/main/java/com/example/it_support/Controller/PersonneController.java
@@ -0,0 +1,13 @@
+package com.example.it_support.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class PersonneController {
+
+}
