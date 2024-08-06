66d952161131494fb8ad5a98e6a0f46351c524c1
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-06 16:27:04 +0100

    Creation PersonneRepository

---
+++ IT_Support/src/main/java/com/example/it_support/Repository/PersonneRepository.java
@@ -0,0 +1,10 @@
+package com.example.it_support.Repository;
+
+import com.example.it_support.Model.PersonneModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PersonneRepository extends JpaRepository<PersonneModel,Long> {
+
+    Optional<PersonneModel> findByEmail(String email);}
