f594125c348f6cf835158dce3e5b42776ee603da
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-08 19:02:02 +0100

    Create TicketDTO

---
+++ IT_Support/src/main/java/com/example/it_support/DTO/TicketDTO.java
@@ -0,0 +1,11 @@
+package com.example.it_support.DTO;
+
+import lombok.*;
+
+@Data
+public class TicketDTO {
+    private  String description;
+    private Long equiId;
+    private Long panneId;
+    private  Long utiliId;
+}
