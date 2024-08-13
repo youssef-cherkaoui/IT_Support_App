d25a97519fcfe519a407871d37830371aeeb4bca
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-08-13 23:29:47 +0100

    create modules

--- IT_Supp_FrontEnd/src/app/app.module.ts
+++ IT_Supp_FrontEnd/src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipementComponent } from './components/equipement/equipement.component';
+import { PanneComponent } from './components/panne/panne.component';
+import { AddEquipementComponent } from './components/add-equipement/add-equipement.component';
+import { ChangerStatusComponent } from './components/changer-status/changer-status.component';
+import { CreatePanneComponent } from './components/create-panne/create-panne.component';
+import { UpdateEquipementComponent } from './components/update-equipement/update-equipement.component';
+import { UpdatePanneComponent } from './components/update-panne/update-panne.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipementComponent,
+    PanneComponent,
+    AddEquipementComponent,
+    ChangerStatusComponent,
+    CreatePanneComponent,
+    UpdateEquipementComponent,
+    UpdatePanneComponent,
+    LoginComponent,
+    HomeComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
